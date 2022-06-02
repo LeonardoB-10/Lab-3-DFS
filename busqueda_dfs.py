92a2bd2ab237562f40e6ee61c4e5ae78cf44d969
Leonardo Borja <vinicio.borja10@gmail.com>
2022-06-02 17:47:37 -0500

    Segundo avance

--- busqueda_dfs.py
+++ busqueda_dfs.py
@@ -1,7 +1,7 @@
 from queue import Queue
 
 class Graph:
-      """
+    """
     Una clase que representa un Grafo
 
     ...
@@ -19,35 +19,53 @@ class Graph:
 
     Métodos
     -------
-    agregando_borde(self, node1, node2, peso=1):
+    agregando_borde(self, nodo1, node2, peso=1):
         Agrega un nuevo grafo
     imprimiendo_lista_adjuntada(self):
          Recorre el diccionario y muestra los datos tanto la llave como el valor 
     bfs_traversal(self, nodo_inicio):
         Imprimir recorrido BFS
     """
-    def __init__(self, num_of_nodes, directed=True):
+    
+    def __init__(self, num_of_nodes, directed=True):#Consstructor con sus parametros
+        """
+        El constructor permite inicializar los atributos del grafo
+
+        Parámetros 
+        -----------
+            numero_de_nodos: int
+                límite de nodos a ingresar
+            dirigido: boolean
+                simetria del grafo 
+
+        """
         self.m_num_of_nodes = num_of_nodes
         self.m_nodes = range(self.m_num_of_nodes)
 		
-        # Directed or Undirected
         self.m_directed = directed
 		
-        # Graph representation - Adjacency list
-        # We use a dictionary to implement an adjacency list
         self.m_adj_list = {node: set() for node in self.m_nodes}      
 	
-    # Add edge to the graph
-    def add_edge(self, node1, node2, weight=1):
-        self.m_adj_list[node1].add((node2, weight))
+
+    def agregando_borde(self, nodo1, node2, weight=1):#agregamos los parametros en la función agregando_borde
+        self.m_adj_list[nodo1].add((node2, weight))
 
         if not self.m_directed:
-            self.m_adj_list[node2].add((node1, weight))
+            self.m_adj_list[node2].add((nodo1, weight))
     
-    # Print the graph representation
-    def print_adj_list(self):
+
+    def imprimiendo_lista_adjuntada(self):
+        """Imprime el diccionario
+        Recorre el diccionario y muestra los datos tanto la llave como el valor 
+
+        Retorna
+        -------
+        Nada
+        """
+        
         for key in self.m_adj_list.keys():
             print("node", key, ": ", self.m_adj_list[key])
+
 
     def dfs(self, start, target, path = [], visited = set()):
         path.append(start)
@@ -64,19 +82,16 @@ class Graph:
 
 
 if __name__ == "__main__":
-    #### EXAMPLE #####
 
-    # Create an instance of the `Graph` class
-    # This graph is undirected and has 5 nodes
     graph = Graph(5, directed=False)
 
-    # Add edges to the graph with default weight=1
+ 
     graph.add_edge(0, 1)
     graph.add_edge(0, 2)
     graph.add_edge(1, 3)
     graph.add_edge(2, 3)
     graph.add_edge(3, 4)
-    # Print adjacency list in the form node n: {(node, weight)}
+
     graph.print_adj_list()
 
     traversal_path = []
